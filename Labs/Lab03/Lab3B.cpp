4c5e72bff4819c60b869076859b69e1498c25cb5
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-10-06 13:49:59 -0400

    Start of Lab3B incomplete

---
+++ Labs/Lab03/Lab3B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string> 
+#include <cmath>
+using namespace std;
+
+double lh, wh;
+char a,b,c;
+
+
+double Perimiter();
+
+void Distance();
+
+string Arrangements();
+
+
+void main()
