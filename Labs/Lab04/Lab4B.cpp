f8df194d26fca0764c97dbe50150396eaa43eff8
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-10-14 10:14:27 -0400

    Lab 4B started

--- Labs/Lab04/Lab4B.cpp
+++ Labs/Lab04/Lab4B.cpp
@@ -1,1 +1,77 @@
-//Empty
+#include <iostream>
+#include <string> 
+#include <cmath>
+using namespace std;
+
+void NextLeapYear(int& B)
+{
+    if (B > 0)
+    {
+        B++;
+    }
+    else if (B < 0)
+    {
+        B = 4;
+    }
+}
+
+string ColorCode(int A)
+{
+    //int A;
+    if (A % 3 == 0)
+    {
+        return "red";
+    }
+    else if (A % 4 == 0)
+    {
+        return "blue";
+    }
+    else if ((A % 4 == 0) && (A % 3 == 0))
+    {
+        return "purple";
+    }
+    else
+    {
+        return "yellow";
+    }
+}
+
+void swap(double x, double y)
+{
+    double tmp = x;
+    x = y;
+    y = tmp;
+}
+
+void Sort4(double& a, double& b, double& c, double& d)
+{
+    //double top, mid, low, bottom;
+
+    if (a <= b)
+    {
+        swap(a,b);
+    }
+    
+    if (c <= d)
+    {
+        swap(c,d);
+    }
+
+    if (a <= c)
+    {
+        swap(a,c);
+    }
+
+    if (b <= d)
+    {
+        swap(b,d);
+    }
+
+    if (b <= c)
+    {
+        swap(b,c);
+    }
+
+    cout<< a << b << c << d;
+}
+
