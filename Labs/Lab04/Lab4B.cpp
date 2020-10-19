6c036a488cab80f6744d3b11af08639d57b47280
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-10-19 02:52:02 -0400

    Lab4B completed.

--- Labs/Lab04/Lab4B.cpp
+++ Labs/Lab04/Lab4B.cpp
@@ -36,42 +36,93 @@ string ColorCode(int A)
     }
 }
 
-void swap(double x, double y)
-{
-    double tmp = x;
-    x = y;
-    y = tmp;
-}
-
 void Sort4(double& a, double& b, double& c, double& d)
 {
-    //double top, mid, low, bottom;
+    double top, mid, low, bottom;
 
-    if (a <= b)
+    if (a > b) //A assigned top
     {
-        swap(a,b);
+        top = a;
+        mid = b;
+    }
+    else //or B assigned top
+    {
+        top = b;
+        mid = a;
+    }
+//Move them down if C is greater than top
+    if (top < c)
+    {
+        low = mid;
+        mid = top;
+        top = c;
+    }
+    else if (mid < c)//if C is not greater than top but is greater than mid, move mid to low and C into Mid
+    {
+        low = mid;
+        mid = c;
+    }
+    else//if C is lowest, move C to 2nd lowest spot
+    {
+        low = c;
     }
     
-    if (c <= d)
+    if (top < d) //Check if D is greater than the top value, and if it is then move all other values down
     {
-        swap(c,d);
+        bottom = low;
+        low = mid;
+        mid = top;
+        top = d;
+    }
+    else if (mid < d) // rinse and repeat prior events
+    {
+        bottom = low;
+        mid = low;
+        mid = d;
+    }
+    else if (low<d)
+    {
+        bottom = low;
+        low = d;
+    }
+    else
+    {
+        bottom = d;
     }
 
-    if (a <= c)
-    {
-        swap(a,c);
-    }
-
-    if (b <= d)
-    {
-        swap(b,d);
-    }
-
-    if (b <= c)
-    {
-        swap(b,c);
-    }
-
-    cout<< a << b << c << d;
+   cout<<endl<<top<<' '<<mid<<' '<<low<<' '<<bottom;
 }
 
+int main()
+{
+    double a,b,c,d;
+    int X,Z;
+
+    cout<<endl<<"Leap Year!";
+    cout<<endl<<"Enter a number, please: ";
+    cin>>Z;
+    NextLeapYear(Z);
+    cout<<Z<<" was returned"<<endl;
+
+    cout<<endl<<"Sort 4!"<<endl;
+    
+    cout<<"Enter the 1st number: "<<endl;
+    cin>> a;
+    cout<<"Enter the 2nd number: "<<endl;
+    cin>> b;
+    cout<<"Enter the 3rd number: "<<endl;
+    cin>>c;
+    cout<<"Enter the 4th number: "<<endl;
+    cin>>d;
+
+    Sort4(a,b,c,d);
+    cout<<endl;
+    cout<<endl<<"Color Code!";
+    cout<<endl<<"Enter an integer: ";
+    cin>> X;
+    cout<<endl<<"The color for your number is "<< ColorCode(X) << "!";
+
+
+return 0;
+
+}
