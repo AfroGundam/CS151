bc606833945056b616bd099f7a1354c1a6007133
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-10-21 09:50:03 -0400

    Quiz 4 complete.

--- Quizzes/Quiz04/quiz04.cpp
+++ Quizzes/Quiz04/quiz04.cpp
@@ -3,8 +3,54 @@
 using namespace std;
 
 //Define Function Here
+int SDPC(int P)
+{
+    int x = 0;
+    if (P%2 == 0)
+    {
+        P = P/2;
+        //cout<<P<<" 2"<<endl;
+        x++;
+    }
+    
+    if (P%3 == 0)
+    {
+        P = P/3;
+        //cout<<P<<" 3"<<endl;
+        x++;
+    }
+    
+    if (P%5 == 0)
+    {
+        P = P/5;
+        //cout<<P<<" 5"<<endl;
+        x++;
+    }
+    
+    if (P%7 == 0)
+    {
+        P = P/7;
+        //cout<<P<<" 7"<<endl;
+        x++;
+    }
+    
+    cout<<x<<endl;
+}
 
 int main()
 {
+    int P;
+    cout<<"Enter number"<<endl;
+    cin>>P;
+    cout<<SDPC(P)<<endl;
+    
+    cout<<"Enter number"<<endl;
+    cin>>P;
+    cout<<SDPC(P)<<endl;
+    
+    cout<<"Enter number"<<endl;
+    cin>>P;
+    cout<<SDPC(P)<<endl;
+
  return 0;
 }
