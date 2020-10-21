ae693a6224bfbba17894f4d30ae8de3e6fa74f88
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-10-21 09:56:26 -0400

    Update quiz04.cpp

--- Quizzes/Quiz04/quiz04.cpp
+++ Quizzes/Quiz04/quiz04.cpp
@@ -6,35 +6,35 @@ using namespace std;
 int SDPC(int P)
 {
     int x = 0;
-    if (P%2 == 0)
+    if (P%2 == 0 && P!=0)
     {
         P = P/2;
         //cout<<P<<" 2"<<endl;
         x++;
     }
     
-    if (P%3 == 0)
+    if (P%3 == 0 && P!=0)
     {
         P = P/3;
         //cout<<P<<" 3"<<endl;
         x++;
     }
     
-    if (P%5 == 0)
+    if (P%5 == 0 && P!=0)
     {
         P = P/5;
         //cout<<P<<" 5"<<endl;
         x++;
     }
     
-    if (P%7 == 0)
+    if (P%7 == 0 && P!=0)
     {
         P = P/7;
         //cout<<P<<" 7"<<endl;
         x++;
     }
     
-    cout<<x<<endl;
+    return x;
 }
 
 int main()
