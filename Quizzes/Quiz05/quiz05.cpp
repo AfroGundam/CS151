cfdf035a0c23cf1f9a6bbd5188ef6c70650957f0
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-11-09 10:28:20 -0500

    Update quiz05.cpp

--- Quizzes/Quiz05/quiz05.cpp
+++ Quizzes/Quiz05/quiz05.cpp
@@ -1,59 +1,76 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//Identify the positions in which each number in the array is at
-//when prompted, user will input a number to assign that number in the array to their proper position in either ascending or decending order
 
-void ProperPosition(int a[], int b)
+bool Monotonic(const int array[])
 {
-    int temp, N, G;
-    N = a[b-1];
-    G = 0;
-    for(int i = 0; i < 5; i += 1)
+    if (array[0] < array[1])//Decending
     {
-        if(a[i]<N)
+        if (array[1]<array[2])
         {
-            G+=1;
+            if (array[2]<array[3])
+            {
+                if (array[3]<array[4])
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            else
+            {
+                return false;
+            }
+            
         }
+        else
+        {
+            return false;
+        }
+        
     }
-    temp = a[G];
-    a[G] = N;
-    a[b-1] = temp;
-
-    //cout <<"C"<<i<<" "<<c[i]<<endl;
+    else if (array[0]>array[1])//Asending
+    {
+        if (array[1]>array[2])
+        {
+            if (array[2]>array[3])
+            {
+                if (array[3]>array[4])
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    else
+    {
+        return false;
+    }
+    
 }
-     
+
+
 int main()
 {
-    int a[5], b, n, size;
-
-    //size = sizeof(a)/sizeof(a[0]);
-    /*
-    cout <<" Please enter 5 single digit numbers for the array";
-    for(int i = 0;i < size ;i += 1)
-    {
-
-    }
-    */
-    //cout << a[];
-    cout <<" Please enter 5 single digit numbers for the array"<<endl;
-    cin>> a[0];
-    cout << endl;
-    cin>> a[1];
-    cout << endl;
-    cin>> a[2];
-    cout << endl;
-    cin>> a[3];
-    cout << endl;
-    cin>> a[4];
-
-    cout << a[0] << " "<< a[1] << " "<< a[2] << " "<< a[3] << " "<< a[4] << endl;
-
-    cout << "Your proper position selection? "<< endl;
-    cin >> b;
-    ProperPosition(a,b);
-
-    cout << a[0] << " "<< a[1] << " "<< a[2] << " "<< a[3] << " "<< a[4] << endl;
-
-    
+    int array[] = {1,2,3,4,5};
+    cout << Monotonic(array) <<endl;
+return 0;
 }
