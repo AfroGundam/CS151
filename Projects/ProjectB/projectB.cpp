09a3ea244eac06a03697ae2f5aab2b64cb08bbe3
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-12-05 03:27:04 -0500

    things

---
+++ Projects/ProjectB/projectB.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+struct deck
+{
+    int count;
+};
+
+struct shore
+{
+    int count;
+    int space;
+};
+
+struct hand
+{
+    int count;
+};
+
+struct card
+{
+
+};
+
+int main()
+{
+
+};
