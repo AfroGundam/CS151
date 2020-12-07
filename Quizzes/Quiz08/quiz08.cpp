51d902c3b5b7d29a22a8636cb3a2cd6f3cd728ca
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-12-07 09:13:34 -0500

    Update quiz08.cpp

--- Quizzes/Quiz08/quiz08.cpp
+++ Quizzes/Quiz08/quiz08.cpp
@@ -3,7 +3,19 @@ int main()
 /*******************************************
 Write solutions below here
 
+	int *s = a;
+	const int *t;
 
+	int * array[20];
+	int *p = array; 
+	struct * q = Point();
+
+
+
+	
+
+	delete p;
+	delete q;
 write olutions above here
 *******************************************/	
 	return 0;
