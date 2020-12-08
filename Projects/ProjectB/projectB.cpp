cb2096682db003a58c4cb674567cd7c34f2b6343
AfroGundam <54641608+AfroGundam@users.noreply.github.com>
2020-12-08 13:55:59 -0500

    DeckShufflin

--- Projects/ProjectB/projectB.cpp
+++ Projects/ProjectB/projectB.cpp
@@ -1,31 +1,112 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>
 using namespace std;
+using std::vector;
+using std::string;
 
 
+enum Rank{ ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE};
+enum Suit{ SPADES, HEARTS, DIAMONDS, CLUBS};
 
-struct deck
+
+struct Card
 {
+    Suit suit;
+    Rank rank;
+    int num_suits = 4;
+    int num_ranks = 13;
+};
+
+struct Deck
+{
+    vector<Card> cards;
+    string card_back;
+    int count;
+    int max = 52;
+};
+
+struct Shore
+{
+    int count;
+    Card* played;
+    int* books;
+    int n_books;
+};
+
+struct Hand
+{
+    Card* CARDS;
     int count;
 };
 
-struct shore
+class Player
 {
-    int count;
-    int space;
+    Hand HAND;
+    Shore field;
 };
 
-struct hand
+class Game
 {
-    int count;
+    Deck CARDS;
+    Player players[4];
 };
 
-struct card
-{
-
-};
+void initialize(Deck&);
+void print_card(const Card&);
+void print_deck(const Deck&);
+void shuffle(Deck&);
 
 int main()
 {
-
+    Deck game_deck;
+    initialize(game_deck);
+    print_deck(game_deck);
+    shuffle(game_deck);
+    cout<<endl;
+    print_deck(game_deck);
 };
+
+void initialize(Deck& deck)
+{
+    Card card;
+    for(int suit = 0; suit < card.num_suits; suit++)
+    {
+        for(int rank = 0; rank < card.num_ranks; rank++)
+        {
+            card.suit = Suit(suit);
+            card.rank = Rank(rank);
+            deck.cards.push_back(card);
+        }
+    }
+}
+
+void print_card(const Card& card)
+{
+    cout<<"Rank = "<< card.rank<<"  "<<"Suit = "<< card.suit << endl;
+}
+
+void print_deck(const Deck& deck)
+{
+    for (Card c : deck.cards)
+    {
+        print_card(c);
+    }
+}
+
+
+
+void shuffle(Deck& deck)
+{
+    Deck shuffled;
+    while(!deck.cards.empty())
+    {
+        size_t rand_index = rand() % deck.cards.size();
+        shuffled.cards.push_back(deck.cards[rand_index]);
+        deck.cards.erase(deck.cards.begin() + rand_index);
+
+    }
+
+    deck = shuffled;
+}
